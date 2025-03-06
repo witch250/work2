3b18ae4dbb9525b4968de57e1d4c2ce1af4d2629
witch250 <1641685226@qq.com>
2025-03-06 21:58:07 +0800

    修bug 移除simhash

--- 3123004768/main.py
+++ 3123004768/main.py
@@ -31,67 +31,7 @@ def WriteTxt(op,path):
         raise FileNotFoundError("找不到文件")
     except PermissionError:
         raise PermissionError("不允许访问文件")
-#某种哈希算法,将词语转为64位2进制数字符串，照搬的，在找出处，2010年前含,有风险，故删除
-@profile
-def hash(source):
-    if source == "":
-            return 0
-    else:
-        #unicode16进制转十进制左移7位
-        x = ord(source[0]) << 7
-        #print (x)
-        m = 1000003
-        mask = 2 ** 128 - 1
-        for c in source:
-            x = ((x * m) ^ ord(c)) & mask
-        x ^= len(source)
-        if x == -1:
-            x = -2
-        #转为64位2进制数
-        x = bin(x).replace('0b', '').zfill(64)[-64:]
-    return str(x)
-    
-@profile
-def SimHash1(txt):
-    taglist=jieba.analyse.extract_tags(txt, topK=20, withWeight=True)
-    T=[]
-    for keyword,weight in taglist:
-        weight*=20
-        weight=math.ceil(weight)
-        #某种哈希算法,将词语转为64位2进制数字符串
-        keyword=hash(keyword)
-        temp=[]
-        #根据64位2进制数字符串和权重构造向量
-        for i in keyword:
-            if i=='1':
-                temp.append(weight)
-            else:
-                temp.append(-weight)
-        T.append(temp)
-    #64单位的20维向量合并为1维向量
-    list1=np.sum(np.array(T),axis=0)
-    if(T==[]):
-        return '00'
-    #64位二进制simhash
-    simhash=''
-    for i in list1:
-        if i<0:
-            simhash+='1'
-        else:
-            simhash+='0'
-    return simhash   
 
-#i=[0,64]10内大概相似,3内认为相似
-@profile
-def hamming(s1,s2):
-    t1='0b'+s1
-    t2='0b'+s2
-    n=int(t1,2)^int(t2,2)
-    i=0
-    while n:
-        n&=(n-1)
-        i+=1
-    return i
 @profile
 def Levenshtein1(txt1,txt2):
     try:
@@ -123,45 +63,30 @@ def Jaccard(txt1,txt2):
     except MemoryError:
         raise MemoryError("内存溢出")
 
-def cmdread():
-    path=sys.argv
-    try:
-        if path[1]=='' or path[2]=='' or path[3]=='':
-            pass
-    except IndexError:
-        raise IndexError("输入不存在")
-    return path 
-
 #main
 if __name__=='__main__':
     try:
-        '''
         #获取路径，更改路径
         os.chdir(os.path.dirname(os.path.abspath(__file__)))
-        print(os.getcwd())
-        path1='./orig.txt'
-        path2='./orig_0.8_add.txt'
-        path3='./output.txt''
-        '''
-        path=cmdread()
-        path1=path[1]
-        path2=path[2]
-        path3=path[3]
+        #print(os.getcwd())
+        path=sys.argv
+        if len(path)>1: #调用cmd
+            try:
+                if path[1]=='' or path[2]=='' or path[3]=='':
+                    pass
+                else:
+                    path1=path[1]
+                    path2=path[2]
+                    path3=path[3]
+            except IndexError:
+                raise IndexError("输入不存在")
+        if len(path)==1: #脚本内参数
+            path1='./orig.txt'
+            path2='./orig_0.8_add.txt'
+            path3='./output.txt'
+        print(path1)
         txt1=OpenTxt(path1)
         txt2=OpenTxt(path2)
-        simhash1=SimHash1(txt1)
-        simhash2=SimHash1(txt2)
-        #print(simhash1)
-        #print(simhash2)
-        result1=hamming(simhash1,simhash2)
-        #print(result1)
-        #拿leven拟合的,数据不够多,R=0.7
-        result1=0.0004*result1*result1-0.039*result1+1
-        if result1<0:
-            result1=0
-        elif result1>1:
-            result=1
-        print(result1)
         result2=Levenshtein1(txt1,txt2)
         print(result2)
         result3=Jaccard(txt1,txt2)
